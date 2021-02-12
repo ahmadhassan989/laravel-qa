0d336f0a26cd38a07f8f51598f5a0f193ab81a13
ahmad.obeidat <ahmad.obeidat@olahub.com>
2021-02-13 01:59:59 +0200

    generat question model

---
+++ app/Question.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Str;
+
+class Question extends Model
+{
+     public function user()
+     {
+         return $this->belongsTo(User::class);
+     }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+
+}
